20f4a40ff4e75fc3e3b80ef2a678a0eeb2c79aad
Jae Kwon <jae@tendermint.com>
2016-03-19 10:31:58 -0700

    Add make update_deps; update basecoin example

--- examples/basecoin/app/init.sh
+++ examples/basecoin/app/init.sh
@@ -2,4 +2,4 @@
 
 go get github.com/tendermint/basecoin/cmd/...
 
-basecoin --addr="unix:///data/tendermint/app/app.sock" --eyes="unix:///data/tendermint/data/data.sock" --genesis="/data/tendermint/app/genesis.json"
+basecoin --address="unix:///data/tendermint/app/app.sock" --eyes="unix:///data/tendermint/data/data.sock" --genesis="/data/tendermint/app/genesis.json"
