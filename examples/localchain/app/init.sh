ba57025426f3388bf8dc484e91c166604d2307d8
Jae Kwon <jae@tendermint.com>
2016-04-20 08:26:57 -0700

    Update basecoin/localchain examples

--- examples/localchain/app/init.sh
+++ examples/localchain/app/init.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
 go get github.com/tendermint/basecoin/cmd/...
+cd $GOPATH/src/github.com/tendermint/basecoin
+git fetch -a origin
+git checkout -b demo
+git reset --hard origin/demo
+make install
 
 basecoin --address="unix:///data/tendermint/app/app.sock" --eyes="unix:///data/tendermint/data/data.sock" --genesis="/data/tendermint/app/genesis.json"
